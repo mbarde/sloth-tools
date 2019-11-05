7ce85628d5ed6e383e75d0161fd2563268eb1cd8
Ubuntu <ubuntu@ubuntu.fritz.box>
2019-11-05 19:00:07 +0000

    allow deployment via waitress

--- README.md
+++ README.md
@@ -10,3 +10,28 @@ sudo apt-get install python3-opencv
 ```
 env FLASK_APP=server.py flask run --host=0.0.0.0
 ```
+
+## Deploy
+
+homecontrol.service
+
+```
+[Unit]
+Description=Homecontrol
+After=network.target
+
+[Service]
+User=ubuntu
+WorkingDirectory=/home/ubuntu/server
+ExecStart=/home/ubuntu/.local/bin/waitress-serve --port=5000 --call "server:create_app"
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Enable:
+
+```
+sudo systemctl enable homecontrol.service
+```
--- server.py
+++ server.py
@@ -8,34 +8,40 @@ import cv2
 import os
 import subprocess
 
-app = Flask(__name__, template_folder='/home/ubuntu/server')
+
+def create_app():
+    app = Flask(__name__, template_folder='/home/ubuntu/server')
+
+    @app.route('/')
+    def index():
+        return render_template('./index.html')
+
+    @app.route('/send')
+    def sendcode():
+        # calls C++
+        code = request.args.get('code', '0')
+        progPath = '/home/ubuntu/433Utils/RPi_utils/codesend'
+        args = ['sudo', progPath, code]
+        FNULL = open(os.devnull, 'w')
+        proc = subprocess.Popen(args, stdout=FNULL)
+        proc.wait()
+        proc = subprocess.Popen(args, stdout=FNULL)
+        proc.wait()
+        proc = subprocess.Popen(args, stdout=FNULL)
+        proc.wait()
+        return f'send: {escape(code)}!'
+
+    @app.route('/webcam')
+    def webcam():
+        capture = cv2.VideoCapture(0)
+        _, frame = capture.read()
+        cv2.imwrite('webcam.jpg', frame)
+        capture.release()
+        return send_file('webcam.jpg', cache_timeout=-1)
+
+    return app
 
 
-@app.route('/')
-def index():
-    return render_template('./index.html')
-
-
-@app.route('/send')
-def sendcode():
-    # calls C++
-    code = request.args.get('code', '0')
-    progPath = '/home/ubuntu/433Utils/RPi_utils/codesend'
-    args = ['sudo', progPath, code]
-    FNULL = open(os.devnull, 'w')
-    proc = subprocess.Popen(args, stdout=FNULL)
-    proc.wait()
-    proc = subprocess.Popen(args, stdout=FNULL)
-    proc.wait()
-    proc = subprocess.Popen(args, stdout=FNULL)
-    proc.wait()
-    return f'send: {escape(code)}!'
-
-
-@app.route('/webcam')
-def webcam():
-    capture = cv2.VideoCapture(0)
-    _, frame = capture.read()
-    cv2.imwrite('webcam.jpg', frame)
-    capture.release()
-    return send_file('webcam.jpg', cache_timeout=-1)
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
