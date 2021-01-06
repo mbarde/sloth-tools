a8175f7a0c593e48d406d47b70975e9cd4c515f7
Matthias Barde <matthiasbarde@gmx.de>
2021-01-06 17:18:18 +0100

    remove certainly NOT debugging logs

--- static/js/queue.js
+++ static/js/queue.js
@@ -11,20 +11,15 @@ class JobQueue {
 
   addJob(promise) {
     if (this.jobs.length >= this.maxJobCount) return
-    console.log('job added')
     this.jobs.push(promise)
-    console.log(this.jobs)
     if (this.jobs.length === 1) this.callNext()
   }
 
   callNext() {
     if (this.jobs.length === 0) {
-      console.log('done!')
       if (this.onAllJobsDone !== null) this.onAllJobsDone()
       return
     }
-    console.log('next job ...')
-    console.log(this.jobs[0])
     this.jobs[0]().then(() => {
       this.jobs.shift()  // remove first item
       if (this.onJobDone !== null) this.onJobDone()
